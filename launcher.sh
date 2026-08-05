10ef632ca16c23ba6c5b91d3c3b70c5a34f8b8ea
kefren68 <75138922+kefren68@users.noreply.github.com>
2026-08-05 18:15:33 +0200

    Version 1.4.5 Stable

--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,4 @@
- #!/bin/sh
+#!/bin/sh
 # 1. Definizione percorsi (FE dentro bin)
 FE_DIR="/sdcard/bin/AGSG_CCF_FE"
 BIN_NAME="launcher"
@@ -10,15 +10,13 @@ chmod +x /tmp/launcher_bin
 export LD_LIBRARY_PATH=$LIBS_DIR:$LD_LIBRARY_PATH
 
 # 3. Congela il menu originale
-killall -9 game game_720p 2>/dev/null
+killall -STOP game 2>/dev/null
 
 # 4. Avvia il Launcher
 cd $FE_DIR
 /tmp/launcher_bin
 
 # 5. Ripristino
-/usr/bin/uiswitch.sh
-
-
+killall -CONT game 2>/dev/null
 
 
